69293773629e9ae345ca13afbeb3ac7dca22fc5d
NicholasDaviesNZ <96546397+NicholasDaviesNZ@users.noreply.github.com>
2023-09-25 14:00:01 +1300

    Update configuration.nix grphics onboard gtx compute

--- workstation/configuration.nix
+++ workstation/configuration.nix
@@ -1,4 +1,4 @@
-# Edit this configuration file to define what should be installed on
+  GNU nano 7.2                                                                                                                                                           /etc/nixos/configuration.nix                                                                                                                                                                      # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -15,6 +15,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.blacklistedKernelModules = ["nouveau"];
+  boot.extraModulePackages = [ pkgs.linuxPackages.nvidia_x11 ];
   networking.hostName = "workstation"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
@@ -43,6 +44,13 @@
     LC_TIME = "en_NZ.UTF-8";
   };
 
+
+  # Configure keymap in X11
+  services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+
+
   # Enable OpenGL
   hardware.opengl = {
     enable = true;
@@ -50,13 +58,7 @@
     driSupport32Bit = true;
   };
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-    videoDrivers = ["nvidia"];
-  };
-
+  services.xserver.videoDrivers = ["amdgpu" "nvidia"];
 
   hardware.nvidia = {
 
@@ -71,15 +73,15 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Do not disable this unless your GPU is unsupported or if you have a good reason to.
     #open = true;
 
     # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
+        # accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
@@ -87,73 +89,6 @@
   };
 
   hardware.nvidia.prime = {
-		# Make sure to use the correct Bus ID values for your syst
-		nvidiaBusId = "PCI:1:0:0";
+                # Make sure to use the correct Bus ID values for your syst
+                nvidiaBusId = "PCI:1:0:0";
   };
-
-  virtualisation.docker.enable = true;
-  virtualisation.docker.enableNvidia = true; 
-
- # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nick = {
-    isNormalUser = true;
-    description = "nick";
-    extraGroups = [ "networkmanager" "wheel" "docker"];
-    packages = with pkgs; [];
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    git
-    lshw
-    #cudatoolkit
-    htop
-    docker
-    docker-compose
-    python3
-    nvidia-docker
-    pciutils
-#    vscode-server
-  ];
-
-
- 
- # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # enable flakes
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
-}
-
-
